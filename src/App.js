69c82c9ed8ae8e0f70e10ac85c21db25f9304064
Eliot <eliotpitman@gmail.com>
2022-05-15 03:56:35 -0500

    so tired

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import "./App.css";
+
+const Person = (props) => {
+  return (
+    <>
+      <h1> Name: {props.name}</h1>
+      <h1> brother: {props.brother}</h1>
+      <h1> last name: jingleheimer</h1>
+      <h1> shit: schmit</h1>
+    </>
+  );
+};
 
 function App() {
   const name = "name";
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
       <div>
+        <Person name={"fucking"} brother={"butthole"} />
         {name ? (
           <>
             {" "}
@@ -16,7 +30,23 @@ function App() {
           </>
         )}{" "}
       </div>
-      <div> {2 + 2} </div>
+      <div>
+        <button
+          onClick={() => {
+            setCounter((prevCount) => prevCount + 1);
+          }}
+        >
+          +
+        </button>
+        <h1>{counter}</h1>
+        <button
+          onClick={() => {
+            setCounter((prevCount) => prevCount - 1);
+          }}
+        >
+          -
+        </button>
+      </div>
     </div>
   );
 }
