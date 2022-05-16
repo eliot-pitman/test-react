3be20d50cc58b60d9a3a22dbc01129e4d9e3f0f5
Eliot <eliotpitman@gmail.com>
2022-05-16 02:32:27 -0500

    finishes basic app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+import MovieCard from "./MovieCard";
+
 // 26600242
 
 const API_URL = "http://www.omdbapi.com/?apikey=26600242&";
 
-const App = () => {
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
+// const movie1 = {
+//   Title: "Blade Runner",
+//   Year: "1982",
+//   imdbID: "tt0083658",
+//   Type: "movie",
+//   Poster:
+//     "https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+// };
 
-    console.log(data);
-  };
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     searchMovies("Blade Runner");
   }, []);
 
-  return <h1> hello </h1>;
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  return (
+    <div className="app">
+      <h1>MovieLAnd</h1>
+
+      <div className="search">
+        <input
+          placeholder="search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        ></input>
+        <button onClick={() => searchMovies(searchTerm)}>Search</button>
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2> No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
