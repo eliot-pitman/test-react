617fd848c4ff407d20af9fb55811b3b73b6ff73b
Eliot <eliotpitman@gmail.com>
2022-05-16 01:11:04 -0500

    adds api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { useEffect } from "react";
+// 26600242
+
+const API_URL = "http://www.omdbapi.com/?apikey=26600242&";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies("Blade Runner");
+  }, []);
+
   return <h1> hello </h1>;
 };
 
